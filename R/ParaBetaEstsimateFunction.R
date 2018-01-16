6a0784b766cd0bac78087ae3a63584100b3829ea
Zhang <andrew.haoyu@gmail.com>
2018-01-16 11:30:02 -0500

    ParaBetaEstimateFunction

--- R/ParaBetaEstsimateFunction.R
+++ R/ParaBetaEstsimateFunction.R
@@ -7,7 +7,7 @@
 #'
 #' @examples
 ParaBetaEstimateFunction <- function(N){
-  fit <-  optim(par = begin,fn=BetaGeometricLikehood,gr=LogL.Derivatives,lower=c(0.0005,0.05),upper=c(0.5,10000),method="L-BFGS-B",control=list(fnscale=-1))
+  fit <-  optim(par = begin,fn=BetaGeometricLikehood,gr=LogL.Derivatives,lower=c(0.0005,0.05),upper=c(0.9995,10000),method="L-BFGS-B",control=list(fnscale=-1))
 
   return(fit$par[1])
 }
