18201836fd4e8c9f473096d26f45e9d55fa44f45
Zhang <andrew.haoyu@gmail.com>
2019-02-10 15:32:13 -0500

    udpate

--- R/ParaBetaEstsimateFunction.R
+++ R/ParaBetaEstsimateFunction.R
@@ -12,4 +12,5 @@
 ParaBetaEstimateFunction <- function(N,cen,begin){
   fit <-  optim(par = begin,fn=function(x){BetaGeometricLikehood(x,N,cen)},gr=function(x){LogL.Derivatives(x,N,cen)},lower=c(0.0005,0.05),upper=c(0.9995,10000),method="L-BFGS-B",control=list(fnscale=-1))
   BetaEst <- fit$par[1]
+  return(list(fit$par,BetaGeometricLikehood(fit$par,N,cen)))
 }
