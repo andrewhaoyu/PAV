c34ef16ffa2d7daaa5cd4d213b24abffc6611b05
Zhang <andrew.haoyu@gmail.com>
2019-02-10 13:43:41 -0500

    npmllogisticfun

--- R/NPMLLogisticFun.R
+++ R/NPMLLogisticFun.R
@@ -26,7 +26,8 @@ Estep <- function(uu,beta,x,y,w,obs){
   pp <- matrix(0,n,n)
   xb <- x%*%beta
   for(i in 1:n){
-pp[i,] <- (1-logit_inver(uu+xb[i]))^(y[i]-obs[i])*logit_inver(uu+xb[i])^obs[i]
+    p_temp = logit_inver(uu+xb[i])
+pp[i,] <- (1-p_temp)^(y[i]-obs[i])*p_temp^obs[i]
 }
   ######### ww perform the bayesian rule
   ######### Pr(Z_j|N_i) = Pr(N_i|Z_j)*Pr(Z_j)/(sum_j Pr(N_i|Z_j)*Pr(Z_j))
@@ -43,6 +44,54 @@ pp[i,] <- (1-logit_inver(uu+xb[i]))^(y[i]-obs[i])*logit_inver(uu+xb[i])^obs[i]
 
   return(ww)
 }
+
+
+
+
+#' Title
+#'
+#' @param y
+#' @param x
+#' @param uu_old
+#' @param beta_old
+#' @param w
+#' @param obs
+#'
+#' @return
+ObsLikfun <- function(y,x,uu_old,beta_old,w,obs){
+  n <- length(y)
+  result <- rep(0,n)
+  xb = x%*%beta_old
+  for(i in 1:n){
+    result[i] <- log(sum(w*logit_inver(uu_old+xb[i])^obs[i]*(1-logit_inver(uu_old+xb[i]))^(y[i]-obs[i])))
+  }
+  return(sum(result))
+
+}
+
+#' Title
+#'
+#' @param y
+#' @param x
+#' @param uu_old
+#' @param beta_old
+#' @param ww
+#' @param obs
+#'
+#' @return
+ComLikfun <- function(y,x,uu_old,beta_old,ww,obs){
+  xb = x%*%beta_old
+  n <- length(y)
+  temp_mat = matrix(0,n,n)
+  for(i in 1:n){
+    temp_mat[i,] = ((uu_old+xb[i])*obs[i]-y[i]*log(1+exp(uu_old+xb[i])))
+  }
+  return(sum(temp_mat*ww))
+}
+
+
+
+
 
 #' gradient decent for M step intercept
 #'
@@ -118,19 +167,31 @@ gr_b_fun <- function(uu,N,ww,b,x,K,obs){
 #' @param obs
 #'
 #' @return
-Mstep <- function(uu,beta,x,y,ww,alpha_x,obs){
-  uu_old <- uu
-  beta_old <- beta
-  step <- 100
+Mstep <- function(uu_old,beta_old,x,y,ww,alpha_x,obs){
+  step <- 200
   n <- length(y)
-  alpha <- 1/n
-  tol <- 0.001
+  alpha <- 1/(n*10)
+  tol <- alpha
+  ComLik0 = ComLikfun(y,x,uu_old,beta_old,ww,obs)
+  #Likelihood_old <- Likelihoodfun(y,x,uu_old,beta_old,ww)
   for(l in 1:step){
 
     uu_beta_old <- c(uu_old,beta_old)
-  #print(uu_beta_old)
+    #print(uu_beta_old)
     uu_new = uu_old+alpha*gr_u_fun(uu_old,y,ww,beta_old,x,n,obs)
-    beta_new <- beta_old+alpha_x*gr_b_fun(uu_new,y,ww,beta_old,x,n,obs)
+    beta_new <- beta_old+(alpha_x/100)*gr_b_fun(uu_new,y,ww,beta_old,x,n,obs)
+    ######test the Likelihood for a few steps
+    if(l%%10==0){
+      ComLik_new = ComLikfun(y,x,uu_new,beta_new,ww,obs)
+      if(ComLik_new>ComLik0){
+        break
+      }else if(ComLik_new<=ComLik0){
+        uu_new = uu_old
+        beta_new = beta_old
+        alpha = alpha/2
+        alpha_x = alpha_x/2
+      }
+    }
     uu_beta_new <- c(uu_new,beta_new)
     error <- max(abs(uu_beta_new-uu_beta_old))
     if(error<tol){
@@ -139,8 +200,9 @@ Mstep <- function(uu,beta,x,y,ww,alpha_x,obs){
     uu_old <- uu_new
     beta_old <- beta_new
   }
-  return(list(uu_new,beta_new))
+  return(list(uu_new,beta_new,l))
 }
+
 
 
 #' NPMLLogFun
@@ -177,14 +239,21 @@ NPMLLogFun <- function(y,x,obs,uu0,beta0){
     alpha_x[i] = alpha_x[i]/var.x[i]
   }
   }
-
+  LikeliResult <- rep(0,step)
+  StepResult <- rep(0,step)
+  #library(PAV)
   for(l in 1:step){
     uu_beta_old <- c(uu_old,beta_old)
     print(uu_beta_old)
-    ww = Estep(uu_old,beta_old,x,y,w)
-    Mstep_result = Mstep(uu_old,beta_old,x,y,ww,alpha_x)
+    #print(uu_beta_old)
+    ww = Estep(uu_old,beta_old,x,y,w,obs)
+    LikeliResult[l] <- ObsLikfun(y,x,uu_old,beta_old,w,obs)
+    #rowSums(ww)
+    Mstep_result = Mstep(uu_old,beta_old,x,y,ww,alpha_x,obs)
+    #Mstep_result = Mstep2(uu_old,beta_old,x,y,ww)
     uu_new = Mstep_result[[1]]
     beta_new = Mstep_result[[2]]
+    #StepResult[l] = Mstep_result[[3]]
     uu_beta_new <- c(uu_new,beta_new)
     error <- max(abs(uu_beta_new-uu_beta_old))
     if(error<tol){
@@ -194,5 +263,8 @@ NPMLLogFun <- function(y,x,obs,uu0,beta0){
     beta_old <- beta_new
     w = colSums(ww)/sum(ww)
   }
-  return(list(uu_new,beta_new))
+  max_likelihood <-   LikeliResult[l]
+  max_step = l
+
+  return(list(uu_new,beta_new,max_likelihood,max_step))
 }
